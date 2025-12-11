02ff53ffca60fee417fb0f90757151cde9c1155b
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-12-11 11:22:52 +0100

    wip

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { useAuth } from "../../contexts/UseAuth";
+import { useAuth } from "../../contexts/useAuth";
 
 const strictEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
