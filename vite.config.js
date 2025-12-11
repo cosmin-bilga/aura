a6e8a40af6d9782d5f37fa8f7d12e7bd36515b6e
Youssef-Touria <touria.youssef@laplateforme.io>
2025-12-11 09:36:55 +0100

    Modification

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost/aura/src",
+        target: "http://localhost/aura/aura/src",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
